cd8df84e776bcec014fa5503bed4e57525448a2f
VagishaKhare <vagishacse131399@gmail.com>
2019-05-18 18:57:57 +0530

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
+import { EditFormComponent } from './edit-form/edit-form.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 
+const appRoutes: Routes = [
+  {
+    path: 'edit-form',
+    component: EditFormComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditFormComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     HttpClientModule
   ],
@@ -16,3 +30,4 @@ import { HttpClientModule} from '@angular/common/http';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
